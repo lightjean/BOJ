58f2d8bfdce02fdb3c187bca82aa37948e34babf
lightjean <hjlee6824@naver.com>
2020-04-01 22:58:36 +0900

    Update 1676.cpp

--- Problem/1676/1676.cpp
+++ Problem/1676/1676.cpp
@@ -7,7 +7,7 @@ int main()
 
     cin >> n;
 
-    while(n > 5 * num) {
+    while(n >= 5 * num) {
         for(int i = 1; i <= n; i++) {
             if(i % (5 * num) == 0) { // 0의 개수는 소인수분해했을 때 10의 개수, 즉 5의 개수와 같음
                 cnt++;
