e7491c429b8c97a722e85dce69ae9517b23abba5
lightjean <hjlee6824@naver.com>
2020-08-30 16:02:42 +0900

    음계

--- Problem/2920/2920.cpp
+++ Problem/2920/2920.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int arr[8];
+    bool ascending = true, descending = true;
 
     for(int i = 0; i < 8; i++) {
         cin >> arr[i];
     }
 
     for(int i = 0; i < 8; i++) {
-        if(arr[i] )
+        if(arr[i] != i + 1) ascending = false;
     }
+
+    if(ascending) {
+        cout << "ascending" << '\n';
+        return 0;
+    }
+
+    for(int i = 0; i < 8; i++) {
+        if(arr[i] != 8 - i) descending = false;
+    }
+
+    if(descending) {
+        cout << "descending" << '\n';
+        return 0;
+    }
+
+    cout << "mixed" << '\n';
+    return 0;
 }
