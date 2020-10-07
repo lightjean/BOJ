2f887bd46c77b233c417d8c0a48b7cad3709d751
lightjean <hjlee6824@naver.com>
2020-10-07 12:16:59 +0900

    Update 11054.cpp

--- Problem/11054/11054.cpp
+++ Problem/11054/11054.cpp
@@ -13,7 +13,7 @@ int main() {
     dp[1][n + 1] = 0;
     dp[0][1] = 1;
     dp[1][n] = 1;
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= n; i++) { // 증가 부분 수열
         int max = 0;
         for (int j = 1; j < i; j++) {
             if (arr[i] > arr[j]) {
@@ -24,7 +24,7 @@ int main() {
         }
         dp[0][i] = max + 1;
     }
-    for (int i = n - 1; i >= 1; i--) {
+    for (int i = n - 1; i >= 1; i--) { // 감소 부분 수열
         int max = 0;
         for (int j = n; j > i; j--) {
             if (arr[i] > arr[j]) {
@@ -36,7 +36,7 @@ int main() {
         dp[1][i] = max + 1;
     }
     int ans = 0;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) { // 합하면 바이토닉 부분 수열
         if (dp[0][i] + dp[1][i] > ans) {
             ans = dp[0][i] + dp[1][i];
         }
