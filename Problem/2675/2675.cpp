398caef68cb5c76dc634a2da21ff6231b3ffbd9d
lightjean <hjlee6824@naver.com>
2020-10-14 10:49:04 +0900

    문자열 반복

--- Problem/2675/2675.cpp
+++ Problem/2675/2675.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    int t, r;
-    string s;
+int main() {
+    int t;
+    string str;
 
     cin >> t;
 
-    while(t--) {
-        int idx = 0;
-
-        cin >> r >> s;
-
-        while(s[idx] != '\0') {
-            for(int i = 0; i < r; i++) {
-                cout << s[idx];
+    while (t--) {
+        int r;
+        cin >> r >> str;
+        for (int i = 0; i < str.size(); i++) {
+            for (int j = 0; j < r; j++) {
+                cout << str[i];
             }
-            idx++;
         }
-
         cout << '\n';
     }
     return 0;
