8dc62239211e4b20cff870ee1636b2967951f117
lightjean <hjlee6824@naver.com>
2020-10-14 10:36:48 +0900

    A+B - 4

--- Problem/10951/10951.cpp
+++ Problem/10951/10951.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
 int main()
 {
     int a, b;
-    while(cin >> a >> b){
-        cout << a+b << endl;
+    while(scanf("%d%d", &a, &b) != EOF){
+        cout << a + b << '\n';
     }
     return 0;
 }
