b0c4d55ea42e6f6bd1af506b1b12202e50b04f24
lightjean <hjlee6824@naver.com>
2020-10-21 17:59:50 +0900

    신기한 수

---
+++ Algora/44주차 연습/17618/17618.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        int tmp = i, sum = 0;
+
+        while (tmp >= 10) {
+            sum += tmp % 10;
+            tmp /= 10;
+        }
+        sum += tmp;
+
+        if (i % sum == 0) cnt++;
+    }
+
+    cout << cnt << '\n';
+    return 0;
+}
