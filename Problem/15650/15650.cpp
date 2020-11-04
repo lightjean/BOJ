8eef8392bb35899f4fb6fe0d20d57ea8a452a84c
lightjean <hjlee6824@naver.com>
2020-11-04 12:29:56 +0900

    N과 M (2)

--- Problem/15650/15650.cpp
+++ Problem/15650/15650.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 int n, m, ans[8];
-bool check[10] = {0};
 
-void run(int index, int n, int m) {
+void run(int index, int start, int n, int m) {
     if (index == m) {
         for (int i = 0; i < m; i++) {
             cout << ans[i] << " ";
@@ -12,18 +11,14 @@ void run(int index, int n, int m) {
         cout << '\n';
         return;
     }
-    for (int i = 1; i <= n; i++) {
-        if (check[i]) continue;
-        if (index >= 1 && ans[index - 1] > i) continue;
+    for (int i = start; i <= n; i++) {
         ans[index] = i;
-        check[i] = 1;
-        run(index + 1, n, m);
-        check[i] = 0;
+        run(index + 1, i + 1, n, m);
     }
 }
 
 int main() {
     cin >> n >> m;
-    run(0, n, m);
+    run(0, 1, n, m);
     return 0;
 }
