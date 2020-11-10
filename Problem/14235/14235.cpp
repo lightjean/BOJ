e47bc5b9a73ed9160f4d717b4d1ee1b8215389b6
lightjean <hjlee6824@naver.com>
2020-11-10 20:18:23 +0900

    크리스마스 선물

---
+++ Problem/14235/14235.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, a;
+    cin >> n;
+
+    priority_queue<int, vector<int>> pq;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (a == 0) {
+            if (!pq.empty()) {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+            else {
+                cout << -1 << '\n';
+            }
+        }
+        else {
+            for (int j = 0; j < a; j++) {
+                int value;
+                cin >> value;
+                pq.push(value);
+            }
+        }
+    }
+
+    return 0;
+}
