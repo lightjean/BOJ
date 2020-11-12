01b01a4dad4054527baa0c1e1ef06368a1f7434d
lightjean <hjlee6824@naver.com>
2020-11-12 10:54:05 +0900

    콘도 선정

---
+++ Algora/47주차 연습/2246/2246.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, d, c, tmp = 10001, ans = 0;
+vector<pair<int, int>> v;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> d >> c;
+        v.push_back({d, c});
+    }
+
+    sort(v.begin(), v.end()); // 거리 순으로 정렬을 해줌
+
+    for (int i = 0; i < n; i++) { // 거리 순으로 정렬을 했기에 거리가 멀어지면 숙박비가 싸지는 것만 세기
+        if (tmp > v[i].second) {
+            tmp = v[i].second;
+            ans += 1;
+        }
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
